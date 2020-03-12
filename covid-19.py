7f51ad67552ef455c1d154cee47d4ba93ebeb324
Lauri Vuorela <lauri.vuorela@gmail.com>
2020-03-12 17:36:46 +0100

    update name

--- covid-19.py
+++ covid-19.py
@@ -48,7 +48,7 @@ def main(settings: dict) -> None:
         message += f"\n\nHittills {total - db_yesterday} nya fall rapporterade idag"
 
     payload = {
-        "username": "New and Improved CoronaBot",
+        "username": "COVID-19",
         "text": message,
         "channel": slack_channel,
         "icon_emoji": ":biohazard_sign:",

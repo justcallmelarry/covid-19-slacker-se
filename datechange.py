06ddb6458cb4ea172a92766e2c83e7135cffb7bd
Lauri Vuorela <lauri.vuorela@gmail.com>
2020-03-18 20:24:42 +0100

    update to use correct keys

--- datechange.py
+++ datechange.py
@@ -3,7 +3,6 @@ import redis
 if __name__ == '__main__':
     db = redis.Redis(host="localhost", port=6379, db=0)
 
-    raw_db_total = db.get("covid-19:total")
-    raw_db_yesterday = db.get("covid-19:yesterday")
+    raw_db_total = db.get("covid-19:current")
 
     db.set("covid-19:yesterday", raw_db_total)

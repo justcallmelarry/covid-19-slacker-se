6723098e7eb37bdbfb9c7530569b9c5a6ad0ca79
Lauri Vuorela <lauri.vuorela@gmail.com>
2020-03-24 14:28:56 +0100

    nicer slack message

--- covid-19.py
+++ covid-19.py
@@ -1,6 +1,7 @@
 import json
 import os.path
 import sys
+from dataclasses import dataclass
 
 import httpx
 import redis
@@ -9,9 +10,60 @@ from bs4 import BeautifulSoup
 DIRPATH = os.path.join(os.path.dirname(__file__))
 
 
-def main(settings: dict, force: bool) -> None:
+@dataclass
+class Covid19:
+    deaths: int
+    infected: int
+    deaths_updated: int
+    infected_updated: int
+    deaths_today: int
+    infected_today: int
+
+    force: bool = False
+
+    def has_updates(self) -> bool:
+        return any([self.deaths_updated, self.infected_updated, force])
+
+
+def slack_message(settings: dict, data: Covid19) -> None:
     slack_webhook = settings.get("slack_webhook")
     slack_channel = settings.get("slack_channel")
+
+    messages = []
+
+    if data.infected_updated:
+        f"{data.infected_updated} rapporterade fall"
+
+    if data.deaths_updated:
+        f"{data.deaths_updated} dödsfall"
+
+    message = " och ".join(messages) + "sedan förra uppdateringen"
+
+    payload = {
+        "link_names": 1,
+        "username": "COVID-19",
+        "channel": slack_channel,
+        "icon_emoji": ":biohazard_sign:",
+        "attachments": [
+            {
+                "title": "Nya uppdateringar",
+                "text": message,
+                "fields": [
+                    {"title": "Rapporterade fall totalt", "value": f"{data.infected}", "short": True},
+                    {"title": "Dödsfall totalt", "value": f"{data.deaths}", "short": True},
+                    {"title": "Rapporterade fall idag", "value": f"{data.infected_today}", "short": True},
+                    {"title": "Dödsfall idag", "value": f"{data.deaths_today}", "short": True},
+                ],
+            }
+        ],
+    }
+
+    response = httpx.post(slack_webhook, data=json.dumps(payload))
+    if response.status_code != 200:
+        print(response.text())
+
+
+def main(settings: dict, force: bool) -> None:
     data_url = settings.get("data_url")
 
     db = redis.Redis(host="localhost", port=6379, db=0)
@@ -25,50 +77,32 @@ def main(settings: dict, force: bool) -> None:
     if not isinstance(db_yesterday, dict):
         db_yesterday = {}
 
-    data = httpx.get(data_url).text
+    response = httpx.get(data_url).text
 
-    page = BeautifulSoup(data, "html.parser")
+    page = BeautifulSoup(response, "html.parser")
 
     infected = int(page.find("span", {"class": "text-danger"}).text)
     # cured = int(page.find("span", {"class": "text-success"}).text)
     deaths = int(page.find("span", {"class": "text-dark"}).text)
 
-    if not any(
-        [infected != db_current.get("infected", 0), deaths != db_current.get("deaths", 0), force]
-    ):
-        return
-
-    message = f"{infected} bekräftat smittade i Sverige"
-
-    diff_infected = infected - db_current.get("infected", 0)
-    if diff_infected:
-        message += f" ({diff_infected} sedan förra uppdateringen)"
-
-    message += f"\nDet har totalt rapporterats {deaths} dödsfall"
-
-    diff_deaths = deaths - db_current.get("deaths", 0)
-    if diff_deaths:
-        message += f" ({diff_deaths} sedan förra uppdateringen)"
-
-    infected_today = infected - db_yesterday.get("infected", 0)
-    deaths_today = deaths - db_yesterday.get("deaths", 0)
-    message += (
-        f"\n\nHittills {infected_today} nya fall och {deaths_today} dödsfall rapporterade idag"
+    data = Covid19(
+        infected=int(page.find("span", {"class": "text-danger"}).text),
+        deaths=int(page.find("span", {"class": "text-dark"}).text),
+        infected_updated=infected - db_current.get("infected", 0),
+        deaths_updated=deaths - db_current.get("deaths", 0),
+        infected_today=infected - db_yesterday.get("infected", 0),
+        deaths_today=deaths - db_yesterday.get("deaths", 0)
     )
 
-    payload = {
-        "username": "COVID-19",
-        "text": message,
-        "channel": slack_channel,
-        "icon_emoji": ":biohazard_sign:",
-    }
+    if force:
+        data.force = True
 
-    response = httpx.post(slack_webhook, data=json.dumps(payload))
+    if not data.has_updates():
+        return
+
+    slack_message(settings, data)
 
     db.set("covid-19:current", json.dumps({"infected": infected, "deaths": deaths}))
-
-    if response.status_code != 200:
-        print(response.text())
 
 
 if __name__ == "__main__":
